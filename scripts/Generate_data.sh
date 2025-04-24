248765b16912c4a0782f431aa17b8b850554c26b
Xi Gao <57949104+xxxxi-17@users.noreply.github.com>
2025-04-24 11:27:16 +0800

    Update Generate_data.sh

--- scripts/Generate_data.sh
+++ scripts/Generate_data.sh
@@ -1,17 +1,17 @@
 ---
 
 # Random_QP_1000_2000
-python generate_data.py --config .\configs\Generate_Data.yaml --prob_type Random_QP --num_var 1000 --num_ineq 2000
+python generate_data.py --config ./configs/Generate_Data.yaml --prob_type Random_QP --num_var 1000 --num_ineq 2000
 
 # Equality_QP_1000_500
-python generate_data.py --config .\configs\Generate_Data.yaml --prob_type Equality_QP --num_var 1000 --num_eq 500
+python generate_data.py --config ./configs/Generate_Data.yaml --prob_type Equality_QP --num_var 1000 --num_eq 500
 
 # SVM_1000_500
-python generate_data.py --config .\configs\Generate_Data.yaml --prob_type Equality_QP --num_var 1000 --num_eq 500
+python generate_data.py --config ./configs/Generate_Data.yaml --prob_type Equality_QP --num_var 1000 --num_eq 500
 
 
 # MM_Q25FV47
-python generate_data.py --config .\configs\Generate_Data.yaml --prob_type MM_Q25FV47
+python generate_data.py --config ./configs/Generate_Data.yaml --prob_type MM_Q25FV47
 
 # MM_QSHIP04L
-python generate_data.py --config .\configs\Generate_Data.yaml --prob_type MM_QSHIP04L
+python generate_data.py --config ./configs/Generate_Data.yaml --prob_type MM_QSHIP04L
