e2e0f506a821507b6754f9d14718dd87ac48e226
Xi Gao <57949104+xxxxi-17@users.noreply.github.com>
2025-05-20 15:03:58 +0800

    Update Generate_data.sh

--- scripts/Generate_data.sh
+++ scripts/Generate_data.sh
@@ -9,9 +9,3 @@ python generate_data.py --config ./configs/Generate_Data.yaml --prob_type Equali
 # SVM_1000_500
 python generate_data.py --config ./configs/Generate_Data.yaml --prob_type Equality_QP --num_var 1000 --num_eq 500
 
-
-# MM_Q25FV47
-python generate_data.py --config ./configs/Generate_Data.yaml --prob_type MM_Q25FV47
-
-# MM_QSHIP04L
-python generate_data.py --config ./configs/Generate_Data.yaml --prob_type MM_QSHIP04L
